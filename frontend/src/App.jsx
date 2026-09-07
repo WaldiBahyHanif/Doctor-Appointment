55930a21ed30b5b42dd23756fea125340d1c4000
Waldi Bahy <waldibahyhaniframadhani@gmail.com>
2026-09-07 19:54:47 +0700

    Persiapan Workspace & Folder Utama

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <h1 className="text-3xl font-bold text-primary">
+        Prescripto Doctor Appointment
+      </h1>
+    </div>
+  );
+};
+
+export default App;
